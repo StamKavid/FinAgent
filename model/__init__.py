ce97ab68f4405da0ef3755e02ae61eb41370e5b6
Stamatis Kavidopoulos <stamkavid@gmail.com>
2024-08-01 22:55:25 +0300

    New changes 1 August 2024

--- model/__init__.py
+++ model/__init__.py
@@ -4,9 +4,10 @@ from .orbit import Orbit
 from .LSTM import MyLSTM
 from .GRU import MyGRU
 from .arima import MyARIMA
-from .prophet import MyProphet
+from .prophet import train_prophet_model
 from .xgboost import MyXGboost
 from .neural_prophet import Neural_Prophet
+from .garch import GARCHModel
 
 
 MODELS = {'random_forest': RandomForest,
@@ -15,7 +16,8 @@ MODELS = {'random_forest': RandomForest,
           'lstm': MyLSTM,
           'gru': MyGRU,
           'arima': MyARIMA,
-          'prophet': MyProphet,
+          'prophet': train_prophet_model,
           'xgboost': MyXGboost,
-          'neural_prophet': Neural_Prophet
+          'neural_prophet': Neural_Prophet,
+          'garch': GARCHModel
           }
